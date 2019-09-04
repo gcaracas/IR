30240999295ac36fea0abe90910de387a908b660
Gerardo Caracas <gerardocaracasur@u.boisestate.edu>
2019-09-03 21:39:31 -0600

    lemmatizer

--- homework2.py
+++ homework2.py
@@ -1,4 +1,8 @@
 from preprocesing import *
 
 pre = preprocessing()
-stem = pre.stemmer(type=porter)
+stem = pre.stemmer(type='porter')
+res=pre.stem(stem=stem, word='Abandon')
+print(res)
+res=pre.stem(stem=stem, word='abandonment')
+print(res)
--- preprocesing.py
+++ preprocesing.py
@@ -1,6 +1,8 @@
 import nltk
-from nltk.stem.porter import *
-nltk.download()
+#from nltk.stem.porter import *
+from nltk.stem import PorterStemmer
+
+#nltk.download()
 class preprocessing:
     def __init__(self):
         print("hello")
@@ -9,5 +11,7 @@ class preprocessing:
         stemmer = None
         if type == 'porter':
             stemmer = PorterStemmer()
-
         return stemmer
+
+    def stem(self, stem='', word=''):
+        return stem.stem(word)
